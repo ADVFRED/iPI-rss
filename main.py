37b866d169fd8c6691157ca353eedfef164b85e2
Liam  Crowley <theadvfred@gmail.com>
2021-01-06 19:28:52 +0000

    Edited .gitignore, finalized cache creation

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import configparser
+from xml.dom import minidom
+import os
 
 #declarations
 PodcastsNames = []
 PodcastsRSS = []
 PodcastCombo = []  #format pc: [('podcast1',podcast1.xml)]
+
 #cfg setup
 config = configparser.ConfigParser()
 config.read("config.cfg")
 
-if config["SETUP"]["ResetCfg"] == True:
-    pass  #reset cfg
+
+if config["SETUP"]["ResetCfg"] == "True":
+	abspath = os.path.abspath(__file__)
+	dname = os.path.dirname(abspath)
+	os.chdir(dname)
+	os.mkdir("xml_cache")
+	config["SETUP"]["ResetCfg"]=False
 
 #cfg parsing
 for name in config["RSS Feeds"]["PodcastNames"].strip("[]").split(","):
@@ -22,4 +30,22 @@ assert len(PodcastsNames) == len(PodcastsRSS)
 for x in range(len(PodcastsNames)):
 	PodcastCombo.append((PodcastsNames[x],PodcastsRSS[x]))
 print(f"{PodcastCombo}")
+
+#Directory setup/verifiction
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+os.chdir("xml_cache")
+
+for pdcstname in PodcastsNames:
+	dirs=dir()
+	if pdcstname in dirs:
+		pass
+	else:
+		os.mkdir(pdcstname)
+		os.chdir(pdcstname)
+		
+
+
 #RSS feed parsing
+#RssXml=minidom.parse(PodcastCombo[0][1])
