4c70663be32977f58c9a0a525d7b402c735255c2
Liam  Crowley <theadvfred@gmail.com>
2021-01-08 18:19:27 +0000

    Finished xml_cache and bugs

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import configparser
 from xml.dom import minidom
 import os
-
+import requests
+import glob
 #declarations
 PodcastsNames = []
 PodcastsRSS = []
@@ -36,16 +37,34 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 os.chdir("xml_cache")
+print(str(os.getcwd()))
+podcastDirectories=[ p.replace('/', '') for p in glob.glob('*/') ]
+print(str(podcastDirectories))
 
 for pdcstname in PodcastsNames:
-	dirs=dir()
-	if pdcstname in dirs:
+	dirs=os.walk(os.getcwd())
+	if pdcstname in podcastDirectories:
 		pass
 	else:
 		os.mkdir(pdcstname)
 		os.chdir(pdcstname)
 		
+#Grab XML files and store to xml_cache
+for podcast in PodcastCombo:
+	abspath = os.path.abspath(__file__)
+	dname = os.path.dirname(abspath)
+	os.chdir(dname+'/xml_cache')
+	#print('start')
+	#os.chdir("/home/runner/iPI-rss/xml_cache")
+	#print(os.getcwd())
+	#print(podcastDirectories)
+	os.chdir(podcast[0])
+	#print(os.getcwd())
+	#This just in, I'm an actual idiot. ProTip: Make sure your print statements are *after* the code your debugging
+	#^^^
 
+	#req=requests.get(podcast[1], allow_redirects=True)
+	#open(podcast[0]+'.xml', 'wb').write(req.content)
 
 #RSS feed parsing
 #RssXml=minidom.parse(PodcastCombo[0][1])
